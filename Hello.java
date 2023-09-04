8b5b65e887147913eccec37ba82542cc84aea727
Prasuna-2004 <cutieprasuna02@gmail.com>
2023-09-04 14:55:18 +0530

    Java file

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 import java.util.*;
 class Hello
 {
-    public static void main(string args[])
+    public static void main(String args[])
     {
-        System.out.println("Hello World")
+        System.out.println("Hello World");
     }
 }
